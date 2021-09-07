3efeade63828dce01720b092a94dbce229378a0e
Nathan <nsrirama@smu.edu>
2021-09-07 17:04:41 -0500

    Initial Commit

---
+++ MSL-Assignment-One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSL-Assignment-One
+//
+//  Created by UbiComp on 9/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
