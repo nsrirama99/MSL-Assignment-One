76d4d951c26658668929db244335883e0c10acc8
williamchan8 <chanw@smu.edu>
2021-09-09 17:05:29 -0500

    Added Stepper functionality on Opening Text

--- MSL-Assignment-One/ViewController.swift
+++ MSL-Assignment-One/ViewController.swift
@@ -8,13 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var entryText: UILabel!
 
+    @IBOutlet weak var stepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        stepper.autorepeat = true
+        stepper.isContinuous = true
+        stepper.minimumValue = 15
+        stepper.maximumValue = 30
+        
+        
+        
     }
 
+    
+    
     @IBAction func startButton(_ sender: Any) {
+    }
+    @IBAction func stepperButton(_ sender: UIStepper) {
+        
+        let stepVal = sender.value
+        entryText.font = entryText.font.withSize(CGFloat(stepVal))
     }
     
 }
