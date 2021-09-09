88ecf17ddf6d10939050e084435babe2c4220bf6
Nathan <nsrirama@smu.edu>
2021-09-09 18:11:58 -0500

    "finished" (I think) the model needed to dynamically load data

--- MSL-Assignment-One/DataModel.h
+++ MSL-Assignment-One/DataModel.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -13,9 +14,26 @@ NS_ASSUME_NONNULL_BEGIN
 
 +(DataModel*)sharedInstance;
 
-@property (nonatomic, strong) NSMutableArray* charNames;
-@property (nonatomic, strong) NSMutableArray* stageNames;
-@property (nonatomic, strong) NSMutableArray* musicNames;
+-(int)loadNames;
+
+-(UIImage*)getImageWithName:(NSString*)name;
+
+-(UIImage*)getCharWithIndex:(NSInteger)index;
+-(UIImage*)getStageWithIndex:(NSInteger)index;
+-(UIImage*)getMusicWithIndex:(NSInteger)index;
+
+-(NSInteger)numberOfChars;
+-(NSInteger)numberOfStages;
+-(NSInteger)numberOfMusic;
+
+-(NSString*)getCharNameForIndex:(NSInteger)index;
+-(NSString*)getStageNameForIndex:(NSInteger)index;
+-(NSString*)getMusicNameForIndex:(NSInteger)index;
+
+
+@property (nonatomic, strong) NSArray* charNames;
+@property (nonatomic, strong) NSArray* stageNames;
+@property (nonatomic, strong) NSArray* musicNames;
 
 @property (nonatomic, strong) NSMutableDictionary* charDict;
 @property (nonatomic, strong) NSMutableDictionary* stageDict;
--- MSL-Assignment-One/DataModel.m
+++ MSL-Assignment-One/DataModel.m
@@ -18,7 +18,7 @@
 
 +(DataModel*)sharedInstance {
     static DataModel* _sharedInstance = nil;
-    static dispatch_once_t predicate = nil;
+    static dispatch_once_t predicate;
     
     dispatch_once(&predicate, ^{
         _sharedInstance = [[DataModel alloc] init];
@@ -29,24 +29,23 @@
 //
 //Array Getters
 //
--(NSMutableArray*)charNames {
+-(NSArray*)charNames {
     if(!_charNames) {
-        
+        [self loadNames];
     }
-    
     return _charNames;
 }
 
--(NSMutableArray*)stageNames {
+-(NSArray*)stageNames {
     if(!_stageNames) {
-        
+        [self loadNames];
     }
     return _stageNames;
 }
 
--(NSMutableArray*)musicNames {
+-(NSArray*)musicNames {
     if(!_musicNames) {
-        
+        [self loadNames];
     }
     return _musicNames;
 }
@@ -56,23 +55,91 @@
 //
 -(NSMutableDictionary*)charDict {
     if(!_charDict) {
+        _charDict = [[NSMutableDictionary alloc] init];
         
+        for(id name in _charNames) {
+            [_charDict setValue:[UIImage imageNamed:name] forKey:name];
+        }
     }
     return _charDict;
 }
 
 -(NSMutableDictionary*)stageDict {
     if(!_stageDict) {
+        _stageDict = [[NSMutableDictionary alloc] init];
         
+        for(id name in _stageNames) {
+            [_stageDict setValue:[UIImage imageNamed:name] forKey:name];
+        }
     }
     return _stageDict;
 }
 
 -(NSMutableDictionary*)musicDict {
     if(!_musicDict) {
+        _musicDict = [[NSMutableDictionary alloc] init];
         
+        for(id name in _musicNames) {
+            [_musicDict setValue:[UIImage imageNamed:name] forKey:name];
+        }
     }
     return _musicDict;
+}
+
+
+-(int)loadNames {
+    NSDictionary* fileDict = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Names" ofType:@"plist"]];
+    
+    _charNames = [fileDict objectForKey:@"Characters"];
+    _stageNames = [fileDict objectForKey:@"Stages"];
+    _musicNames = [fileDict objectForKey:@"Music"];
+    
+    return 1;
+}
+
+-(UIImage*)getImageWithName:(NSString*)name {
+    UIImage* image = nil;
+    
+    if([_charDict objectForKey:name])
+        image = _charDict[name];
+    else if ([_stageDict objectForKey:name])
+        image = _stageDict[name];
+    else
+        image = _musicDict[name];
+    
+    return image;
+}
+
+-(UIImage*)getCharWithIndex:(NSInteger)index {
+    NSString* name = [_charNames objectAtIndex:index];
+    
+    return _charDict[name];
+}
+
+-(UIImage*)getStageWithIndex:(NSInteger)index {
+    NSString* name = [_stageNames objectAtIndex:index];
+    
+    return _stageDict[name];
+}
+
+-(UIImage*)getMusicWithIndex:(NSInteger)index {
+    NSString* name = [_musicNames objectAtIndex:index];
+    
+    return _musicDict[name];
+}
+
+-(NSInteger)numberOfChars { return _charNames.count; }
+-(NSInteger)numberOfStages { return _stageNames.count; }
+-(NSInteger)numberOfMusic { return _musicNames.count; }
+
+-(NSString*)getCharNameForIndex:(NSInteger)index {
+    return _charNames[index];
+}
+-(NSString*)getStageNameForIndex:(NSInteger)index {
+    return _stageNames[index];
+}
+-(NSString*)getMusicNameForIndex:(NSInteger)index {
+    return _musicNames[index];
 }
 
 @end
