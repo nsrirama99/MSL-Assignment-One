a5ca7ae59b25275afe8337c8d43a46b62d866a42
Anna Iorio <annaiorio@Annas-MacBook-Pro.local>
2021-09-10 20:23:57 -0500

    Completed Segmented View Controller

---
+++ MSL-Assignment-One/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  MSL-Assignment-One
+//
+//  Created by Anthony Wang on 9/10/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MSL-Assignment-One/SegmentViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SegmentViewController.swift
+//  MSL-Assignment-One
+//
+//  Created by Anna Iorio on 9/10/21.
+//
+
+import UIKit
+
+class SegmentViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var uiView: UIView!
+    
+    var displayImageName = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func segmentDidChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            imageView.image = UIImage(named: displayImageName)
+            break
+        case 1:
+            imageView.image = UIImage(named: displayImageName + "-rotated")
+            break
+        default:
+            break
+        }
+    }
+    
+    @IBAction func switchDidChange(_ sender: UISwitch) {
+        if (sender.isOn) {
+            uiView.backgroundColor = UIColor.red
+        }
+        else {
+            uiView.backgroundColor = UIColor.white
+        }
+    }
+    
+}
